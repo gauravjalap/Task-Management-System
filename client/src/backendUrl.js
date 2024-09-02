95716978f1e425d62a9313bb1db8fa991262e5a3
gauravjalap <luckyskchirag@gmail.com>
2024-09-02 07:31:52 +0530

    backendUrl changed

--- client/src/backendUrl.js
+++ client/src/backendUrl.js
@@ -1,1 +1,2 @@
-export const backendUrl = 'https://task-management-system-server-smoky.vercel.app';
+// export const backendUrl = 'https://task-management-system-server-smoky.vercel.app';
+export const backendUrl = "http://localhost:5000";
